3e63a2c81a9957ab55ecfd90a4ae64f877f4c50e
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-10-23 11:35:06 +0300

    Update prog.c

--- prog.c
+++ prog.c
@@ -1,33 +1,32 @@
+#include <stdlib.h>
 #include <stdio.h>
 int main()
 {
-    printf("Vvedite kolvo elementov\n");
-    int k;
-    scanf("%d",&k);
-    printf("Vvedite massiv\n");
-    int a[k];
-    int flag = 1;
-    for (int i=0; i<k; i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for (int i=0; (i<k-1) && (flag == 1); i++)
-    {
-        flag = 0;
-        for (int j=0; (j<k-i-1); j++)
-        {
-            if (a[j+1]<a[j])
-            {
-                int b=a[j+1];
-                a[j+1]=a[j];
-                a[j]=b;
-                flag = 1;
-            }
-        }
-    }
-    for (int i=0; i<k; i++)
-    {
-        printf("%d ",a[i]);
-    }
-    return 0;
+	printf("Vvedite massiv\n");
+	int k = 10;
+	int a[10] = {0};
+	int flag = 1;
+	for (int i = 0; i < k; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+	for (int i = 0; (i < k - 1) && (flag == 1); i++)
+	{
+		flag = 0;
+		for (int j = 0; j < k - i - 1; j++)
+		{
+			if (a[j + 1] < a[j])
+			{
+				int b = a[j + 1];
+				a[j + 1] = a[j];
+				a[j] = b;
+				flag = 1;
+			}
+		}
+	}
+	for (int i = 0; i < k; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	return 0;
 }
